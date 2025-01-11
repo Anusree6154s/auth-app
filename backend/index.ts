0acc5db057c8ab33b37825c12f8537b951630695
Anusree <anilkumaranusree113@gmail.com>
2025-01-11 15:17:13 +0530

    test deploy

--- backend/index.ts
+++ backend/index.ts
@@ -39,7 +39,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: "none",
+      sameSite: true,
       maxAge: 1000 * 60 * 60 * 24, // 24 hours in milliseconds
       httpOnly: false, // lets client-side JS access the cookie,
       secure: true, //allows cookie transfer only on https
