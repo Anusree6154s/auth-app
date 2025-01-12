62c58e140dffc43f2df009c156ed90b2aec01313
Anusree <anilkumaranusree113@gmail.com>
2025-01-12 18:38:22 +0530

    fix(backend):edit scripts

--- backend/build/config/constants.js
+++ backend/build/config/constants.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.google_refresh_token = exports.senders_gmail_app_password = exports.senders_gmail = exports.redis_url = exports.jwt_secret = exports.github_client_secret = exports.github_client_id = exports.twitter_api_secret = exports.twitter_api_key = exports.facebook_client_secret = exports.facebook_client_id = exports.google_client_secret = exports.google_client_id = exports.session_secret = exports.port = void 0;
+exports.google_refresh_token = exports.senders_gmail_app_password = exports.senders_gmail = exports.redis_url = exports.jwt_secret = exports.github_client_secret = exports.github_client_id = exports.twitter_api_secret = exports.twitter_api_key = exports.facebook_client_secret = exports.facebook_client_id = exports.google_client_secret = exports.google_client_id = exports.session_secret = exports.env = exports.port = void 0;
 require("dotenv/config");
 exports.port = process.env.PORT || 3000;
+exports.env = process.env.NODE_ENV || "";
 exports.session_secret = process.env.session_secret || "";
 exports.google_client_id = process.env.google_client_id || "";
 exports.google_client_secret = process.env.google_client_secret || "";
